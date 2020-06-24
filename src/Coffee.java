a3315cd982f3679c5cd90621fabe805269c917ba
forynski <65871510+forynski@users.noreply.github.com>
2020-06-24 21:47:42 +0100

    Create Coffee.java

---
+++ src/Coffee.java
@@ -0,0 +1,34 @@
+package machine;
+
+public class Coffee {
+    private String name;
+    private int water;
+    private int milk;
+    private int beans;
+    private int price;
+
+
+    public Coffee(String name, int water, int milk, int beans, int price) {
+        this.name = name;
+        this.water = water;
+        this.milk = milk;
+        this.beans = beans;
+        this.price = price;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public int getBeans() {
+        return beans;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
